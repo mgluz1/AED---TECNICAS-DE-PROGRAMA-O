f8c0469e1dfcdb08434cd8831b741619790ef625
mgluz1 <miguelmluz777@gmail.com>
2025-12-08 23:17:14 -0300

    Add header file for mathematical functions

---
+++ matematica.h
@@ -0,0 +1,12 @@
+#ifndef MATEMATICA_H
+#define MATEMATICA_H
+
+int fatorial(int n);
+int eh_primo(int n);
+int mdc(int a, int b);
+int mmc(int a, int b);
+double media(double a, double b);
+double potencia(double base, int expoente);
+double raiz_quadrada(double n);
+
+#endif
