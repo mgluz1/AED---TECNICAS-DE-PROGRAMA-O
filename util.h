3450e0bf6f319b1e77d70853b3f82008f85b5f4c
mgluz1 <miguelmluz777@gmail.com>
2025-12-08 23:19:30 -0300

    Create util.h

---
+++ util.h
@@ -0,0 +1,10 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+void limpa_buffer();
+void pausa();
+int numero_aleatorio(int min, int max);
+void ordena_vetor(int *vetor, int tamanho);
+void imprime_vetor(const int *vetor, int tamanho);
+
+#endif
