aef4757e0cd7c03c133ba7439d1ae54d95e9e5c6
mgluz1 <miguelmluz777@gmail.com>
2025-12-08 23:18:32 -0300

    Add strings.h header file with string functions

---
+++ strings.h
@@ -0,0 +1,11 @@
+#ifndef STRINGS_H
+#define STRINGS_H
+
+int conta_vogais(const char *str);
+int conta_consoantes(const char *str);
+void string_maiuscula(char *str);
+void string_minuscula(char *str);
+void inverte_string(char *str);
+int compara_strings(const char *s1, const char *s2);
+
+#endif
