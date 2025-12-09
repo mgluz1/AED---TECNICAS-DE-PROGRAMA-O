7433ac8e8847f63bd37c53c41650cd00e0cfe460
mgluz1 <miguelmluz777@gmail.com>
2025-12-08 23:16:49 -0300

    Implement mathematical functions in matematica.c

---
+++ matematica.c
@@ -0,0 +1,49 @@
+#include "matematica.h"
+
+int fatorial(int n) {
+    if (n < 0) return -1;
+    int fat = 1;
+    for (int i = 1; i <= n; i++)
+        fat *= i;
+    return fat;
+}
+
+int eh_primo(int n) {
+    if (n <= 1) return 0;
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0) return 0;
+    return 1;
+}
+
+int mdc(int a, int b) {
+    while (b != 0) {
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+int mmc(int a, int b) {
+    return (a * b) / mdc(a, b);
+}
+
+double media(double a, double b) {
+    return (a + b) / 2.0;
+}
+
+double potencia(double base, int expoente) {
+    double resultado = 1.0;
+    for (int i = 0; i < expoente; i++)
+        resultado *= base;
+    return resultado;
+}
+
+double raiz_quadrada(double n) {
+    double x = n, y = 1.0;
+    while ((x - y) > 0.000001 || (y - x) > 0.000001) {
+        x = (x + y) / 2;
+        y = n / x;
+    }
+    return x;
+}
